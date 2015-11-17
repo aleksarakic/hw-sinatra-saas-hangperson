8e4ea11b6ec9dc1ef1971d920f062571d1a388fc
Aleksa <rakicaleksa@ymail.com>
2015-11-17 22:18:35 +0000

    Ready for Heroku.

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -1,15 +1,13 @@
 class HangpersonGame
-
-  # add the necessary class methods, attributes, etc. here
-  # to make the tests in spec/hangperson_game_spec.rb pass.
-
-  # Get a word from remote "random word" service
-
-  # def initialize()
-  # end
-  
+  attr_accessor :word, :guesses, :wrong_guesses
+  attr_reader :word_with_guesses
   def initialize(word)
     @word = word
+    @guesses = ""
+    @wrong_guesses = ""
+    @guessed = []
+    @word_with_guesses = word.gsub(/[a-z]/i, "-")
+    @count_wrong = 0
   end
 
   def self.get_random_word
@@ -18,5 +16,39 @@ class HangpersonGame
     uri = URI('http://watchout4snakes.com/wo4snakes/Random/RandomWord')
     Net::HTTP.post_form(uri ,{}).body
   end
-
+  
+  def guess(char)
+    if not char or char =~ /([^a-z]|^$)/i
+      raise ArgumentError
+    end
+    char.downcase!
+    if @word.include? char
+      @wrong_guesses = ''
+      @guesses = char
+    else
+      @guesses = ''
+      @wrong_guesses = char
+    end
+    return false if @guessed.include? char
+    @count_wrong += 1 if @wrong_guesses != ''
+    update_word_with_guesses char
+    @guessed << char
+    
+  end
+  
+  def check_win_or_lose
+    return :lose if @count_wrong >= 7
+    return :win if @word == @word_with_guesses
+    :play
+  end
+  
+  
+  private
+  def update_word_with_guesses char
+    cur_word_list = @word_with_guesses.split ''
+    (0..@word.length-1).each do |idx|
+      cur_word_list[idx] = @word[idx] if @word[idx] == char
+    end
+    @word_with_guesses = cur_word_list.join
+  end
 end
